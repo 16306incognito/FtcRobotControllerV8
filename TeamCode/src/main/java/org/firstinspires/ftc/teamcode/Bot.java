ee5d844077a71730fde0c70493bb593f7cebcb4a
eipiequals-1 <olivier.lvannier@gmail.com>
2022-11-21 15:30:47 -0800

    removed unnecessary files, added 2 gamepad support + bot movement

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot.java
@@ -3,12 +3,14 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.mech.ArmSubSystem;
+import org.firstinspires.ftc.teamcode.mech.MovementSubSystem;
 import org.firstinspires.ftc.teamcode.subsystem.SubSystemOpMode;
 
 @TeleOp(name="Bot", group="Linear Opmode")
 public class Bot extends SubSystemOpMode {
 
 	private ArmSubSystem armSubSystem;
+	private MovementSubSystem movementSubSystem;
 
 	public Bot() {
 		super();
@@ -17,7 +19,10 @@ public class Bot extends SubSystemOpMode {
 	@Override
 	public void init() {
 		armSubSystem = new ArmSubSystem();
+		movementSubSystem = new MovementSubSystem();
+		// add the subsystems
 		addSubSystem(armSubSystem);
+		addSubSystem(movementSubSystem);
 
 		super.init();
 	}
